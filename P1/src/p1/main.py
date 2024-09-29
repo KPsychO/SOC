dcbd184636c135f1fc27489c208084eb45215ceb
PsychO <datanc01@ucm.es>
2024-09-29 23:58:30 +0200

    Done?
    needs refactoring and parametrizing but it works for the NEW posts on the subreddit

--- P1/src/p1/main.py
+++ P1/src/p1/main.py
@@ -8,35 +8,28 @@ base_url = "https://old.reddit.com/r/spain/new/"
 # user_comments_url = "https://old.reddit.com/user/" + user + "/comments/"
 
 # print("Processing posts from " + base_url + ", please wait...")
-
-# res = requests.get(base_url)
-# html = BeautifulSoup(res.text, "html.parser")
-
-# siteTable = html.find("div", attrs={"id":"siteTable"})
-
-# posts = siteTable.findAll("div", attrs={"class":"link"})
-
-# users = []
-# for post in posts:
-#     # print(post)
-#     user = post.find("a", attrs={"class":"author"})
-#     if user != None:
-#         # print(user.text)
-#         users.append(user.text)
-
-# print(users)
+res = requests.get(base_url)
+html = BeautifulSoup(res.text, "html.parser")
+siteTable = html.find("div", attrs={"id":"siteTable"})
+posts = siteTable.findAll("div", attrs={"class":"link"})
+users = []
+for post in posts:
+    # print(post)
+    user = post.find("a", attrs={"class":"author"})
+    if user != None:
+        # print(user.text)
+        users.append(user.text)
+print(users)
 # print("Processing users, please wait...")
 
-users = ['Vegetable-Read-4091', 'Inkinidas', 'nothereforthatlong', 'MeCagoEnPeronconga', 'bimbochungo', 'DiegoDGD', 'pabloquest', 'nothereforthatlong', 'afonsoeans', 'theclash8', 'AdAstra6767', 'NeoTheMan24', 'birrakilmister', 'dcarrero', 'paniniconqueso', 'Pacman_242', 'frurtainspeese', 'DiegoDGD', 'paniniconqueso', 'chthroka', 'advocado20', 'rex-ac']
-
-#users = ['theclash8']
-
+users = list(dict.fromkeys(users))
+data = []
 for user in users:
-    print("Scrapping " + user + "...")
+    #print("Scrapping " + user + "...")
     user_url = "https://old.reddit.com/user/" + user
     # print(user_url)
     time.sleep(1)
-    print(user_url)
+    #print(user_url)
     res = requests.get(user_url)
     html = BeautifulSoup(res.text, "html.parser")
 
@@ -45,21 +38,22 @@ for user in users:
     # Karma
     side = html.find("div", attrs={"class":"side"})
 
+    karma = 0
     if side != None:
         post_karma = side.find("span", attrs={"class":"karma"}).text
         comment_karma = side.find("span", attrs={"class":"comment-karma"}).text
 
         karma = int(post_karma.replace(',','')) + int(comment_karma.replace(',',''))
 
-        print(karma)
-    else:
-        print("[ERROR]: No se pudo acceder a: " + user_url)
+#    else:
+        #print("[ERROR]: No se pudo acceder a: " + user_url)
 
+    posts = []
+    comments = []
     # Posts & Comments
     if side != None:
         siteTable = html.find("div", attrs={"id":"siteTable"})
-        posts = []
-        comments = []
+
 
         if siteTable != None:
             things = siteTable.findAll("div", attrs={"class":"thing"})
@@ -71,38 +65,70 @@ for user in users:
                         title_all = top_matter.find("a", attrs={"class":"title"})
                         title = title_all.text
                         desc = ""
+                        tagline = top_matter.find("p", attrs={"class":"tagline"})
+                        date = tagline.find("time")['title']
 
-                        pattern = re.compile("\/r\/spain\/.*")
+                        pattern = re.compile("/r/spain/.*")
                         if pattern.match(title_all['href']):
                             url_post = "https://old.reddit.com" + title_all['href']
-                            tagline = top_matter.find("p", attrs={"class":"tagline"})
-                            date = tagline.find("time")['title']
 
                             time.sleep(1)
-                            print(url_post)
+                            # print(url_post)
                             res_post = requests.get(url_post)
                             html_post = BeautifulSoup(res_post.text, "html.parser")
 
-                            expando = html_post.find("div", attrs={"class": ["expando"]})
+                            expando = html_post.find("div", attrs={"class": "expando"})
                             if expando != None:
                                 md = expando.find("div", attrs={"class":"md"})
                                 if md != None:
                                     desc = md.text
                         
-                        posts.append(
-                            {
-                                "title" : title,
-                                "post_url": url_post,
-                                "date": date,
-                                "desc": desc,
-                                "author": user
-                            }
-                        )
+                            posts.append(
+                                {
+                                    "title" : title,
+                                    "post_url": url_post,
+                                    "date": date,
+                                    "desc": desc,
+                                    "author": user
+                                }
+                            )
 
                     elif thing['data-type'] == "comment":
-                        print("Comentario!")
+                        parent = thing.find("p", attrs={"class":"parent"})
+                        if parent != None:
+                            title = parent.find("a", attrs={"class":"title"})
+                            post = title['href']
+
+                            entry = thing.find("div", attrs={"class":"entry"})
+
+                            tagline = entry.find("p", attrs={"class":"tagline"})
+                            date = tagline.find("time")['title']
+
+                            usertext_body = entry.find("div", attrs={"class":"usertext-body"})
+
+                            if usertext_body != None:
+                                md = usertext_body.find("div", attrs={"class":"md"})
+                                if md != None:
+                                    text = md.text
+
+                                    comments.append(
+                                        {
+                                            "text": text,
+                                            "date": date,
+                                            "parent": post,
+                                            "author": user
+                                        }
+                                    )
                     else:
                         print("[ERROR]: No es ni post ni comentario")
 
-        print(posts)
+    data.append(
+        {
+            "username": user,
+            "karma": karma,
+            "posts": posts,
+            "comments": comments
+        }
+    )
 
+print(data)
