4f2a344a41a839ca686b8d29657f7605a81805be
PsychO <datanc01@ucm.es>
2024-09-30 00:19:23 +0200

    tenemos funcionesgit add src/p1/main.py

--- P1/src/p1/main.py
+++ P1/src/p1/main.py
@@ -3,125 +3,119 @@ from bs4 import BeautifulSoup
 import time
 import re
 
-base_url = "https://old.reddit.com/r/spain/new/"
-# user_url = "https://old.reddit.com/user/" + user
-# user_comments_url = "https://old.reddit.com/user/" + user + "/comments/"
+def scrap_posts(posts, thing):
+    top_matter = thing.find("div", attrs={"class": "top-matter"})
+    title_all = top_matter.find("a", attrs={"class": "title"})
+    title = title_all.text
+    desc = ""
+    tagline = top_matter.find("p", attrs={"class": "tagline"})
+    date = tagline.find("time")["title"]
 
-# print("Processing posts from " + base_url + ", please wait...")
-res = requests.get(base_url)
-html = BeautifulSoup(res.text, "html.parser")
-siteTable = html.find("div", attrs={"id": "siteTable"})
-posts = siteTable.findAll("div", attrs={"class": "link"})
+    pattern = re.compile("/r/spain/.*")
+    if pattern.match(title_all["href"]):
+        url_post = "https://old.reddit.com" + title_all["href"]
+
+        time.sleep(1)
+        res_post = requests.get(url_post)
+        html_post = BeautifulSoup(res_post.text, "html.parser")
+
+        expando = html_post.find("div", attrs={"class": "expando"})
+        if expando != None:
+            md = expando.find("div", attrs={"class": "md"})
+            if md != None:
+                desc = md.text
+
+        posts.append(
+            {
+                "title": title,
+                "post_url": url_post,
+                "date": date,
+                "desc": desc,
+                "author": user,
+            }
+        )
+
+def scrap_comments(comments, thing):
+    parent = thing.find("p", attrs={"class": "parent"})
+    if parent != None:
+        title = parent.find("a", attrs={"class": "title"})
+        post = title["href"]
+
+        entry = thing.find("div", attrs={"class": "entry"})
+
+        tagline = entry.find("p", attrs={"class": "tagline"})
+        date = tagline.find("time")["title"]
+
+        usertext_body = entry.find(
+            "div", attrs={"class": "usertext-body"}
+        )
+
+        if usertext_body != None:
+            md = usertext_body.find("div", attrs={"class": "md"})
+            if md != None:
+                text = md.text
+
+                comments.append(
+                    {
+                        "text": text,
+                        "date": date,
+                        "parent": post,
+                        "author": user,
+                    }
+                )
+
+def obtain_users_list_from_url(users, base_url):
+    res = requests.get(base_url)
+    html = BeautifulSoup(res.text, "html.parser")
+    siteTable = html.find("div", attrs={"id": "siteTable"})
+    posts = siteTable.findAll("div", attrs={"class": "link"})
+    for post in posts:
+        user = post.find("a", attrs={"class": "author"})
+        if user != None:
+            users.append(user.text)
+
+def obtain_user_karma(karma, html):
+    side = html.find("div", attrs={"class": "side"})
+    if side != None:
+        post_karma = side.find("span", attrs={"class": "karma"}).text
+        comment_karma = side.find("span", attrs={"class": "comment-karma"}).text
+        karma = int(post_karma.replace(",", "")) + int(comment_karma.replace(",", ""))
+
+
+base_url = "https://old.reddit.com/r/spain/new/"
+
 users = []
-for post in posts:
-    # print(post)
-    user = post.find("a", attrs={"class": "author"})
-    if user != None:
-        # print(user.text)
-        users.append(user.text)
+obtain_users_list_from_url(users, base_url)
 print(users)
-# print("Processing users, please wait...")
+
 
 users = list(dict.fromkeys(users))
 data = []
 for user in users:
-    # print("Scrapping " + user + "...")
     user_url = "https://old.reddit.com/user/" + user
-    # print(user_url)
     time.sleep(1)
-    # print(user_url)
     res = requests.get(user_url)
     html = BeautifulSoup(res.text, "html.parser")
 
-    # print(html)
-
     # Karma
-    side = html.find("div", attrs={"class": "side"})
-
     karma = 0
-    if side != None:
-        post_karma = side.find("span", attrs={"class": "karma"}).text
-        comment_karma = side.find("span", attrs={"class": "comment-karma"}).text
+    obtain_user_karma(karma, html)
 
-        karma = int(post_karma.replace(",", "")) + int(comment_karma.replace(",", ""))
-
-    #    else:
-    # print("[ERROR]: No se pudo acceder a: " + user_url)
-
+    # Posts & Comments
     posts = []
     comments = []
-    # Posts & Comments
-    if side != None:
-        siteTable = html.find("div", attrs={"id": "siteTable"})
+    
+    siteTable = html.find("div", attrs={"id": "siteTable"})
 
-        if siteTable != None:
-            things = siteTable.findAll("div", attrs={"class": "thing"})
+    if siteTable != None:
+        things = siteTable.findAll("div", attrs={"class": "thing"})
 
-            for thing in things:
-                if thing["data-subreddit"] == "spain":
-                    if thing["data-type"] == "link":
-                        top_matter = thing.find("div", attrs={"class": "top-matter"})
-                        title_all = top_matter.find("a", attrs={"class": "title"})
-                        title = title_all.text
-                        desc = ""
-                        tagline = top_matter.find("p", attrs={"class": "tagline"})
-                        date = tagline.find("time")["title"]
-
-                        pattern = re.compile("/r/spain/.*")
-                        if pattern.match(title_all["href"]):
-                            url_post = "https://old.reddit.com" + title_all["href"]
-
-                            time.sleep(1)
-                            # print(url_post)
-                            res_post = requests.get(url_post)
-                            html_post = BeautifulSoup(res_post.text, "html.parser")
-
-                            expando = html_post.find("div", attrs={"class": "expando"})
-                            if expando != None:
-                                md = expando.find("div", attrs={"class": "md"})
-                                if md != None:
-                                    desc = md.text
-
-                            posts.append(
-                                {
-                                    "title": title,
-                                    "post_url": url_post,
-                                    "date": date,
-                                    "desc": desc,
-                                    "author": user,
-                                }
-                            )
-
-                    elif thing["data-type"] == "comment":
-                        parent = thing.find("p", attrs={"class": "parent"})
-                        if parent != None:
-                            title = parent.find("a", attrs={"class": "title"})
-                            post = title["href"]
-
-                            entry = thing.find("div", attrs={"class": "entry"})
-
-                            tagline = entry.find("p", attrs={"class": "tagline"})
-                            date = tagline.find("time")["title"]
-
-                            usertext_body = entry.find(
-                                "div", attrs={"class": "usertext-body"}
-                            )
-
-                            if usertext_body != None:
-                                md = usertext_body.find("div", attrs={"class": "md"})
-                                if md != None:
-                                    text = md.text
-
-                                    comments.append(
-                                        {
-                                            "text": text,
-                                            "date": date,
-                                            "parent": post,
-                                            "author": user,
-                                        }
-                                    )
-                    else:
-                        print("[ERROR]: No es ni post ni comentario")
+        for thing in things:
+            if thing["data-subreddit"] == "spain":
+                if thing["data-type"] == "link":
+                    scrap_posts(posts, thing)
+                elif thing["data-type"] == "comment":
+                    scrap_comments(comments, thing)
 
     data.append(
         {"username": user, "karma": karma, "posts": posts, "comments": comments}
