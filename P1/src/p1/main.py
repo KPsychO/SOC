9422a0202acf175e5d414af6fb783706207d6a77
PsychO <datanc01@ucm.es>
2024-10-01 02:13:01 +0200

    Implementado retries en caso de error en la request y formateo/refactorizacion de codigo

--- P1/src/p1/main.py
+++ P1/src/p1/main.py
@@ -1,9 +1,22 @@
 import requests
+from requests.adapters import HTTPAdapter, Retry
 from bs4 import BeautifulSoup
 import time
 import re
 
-def scrap_user_posts(posts, thing, user): # Process data from the post published by the user on r/spain
+# Configure a requests session to implement retries in case of an invalid server response
+ses = requests.Session()
+retries = Retry(total=5, backoff_factor=0.5, status_forcelist=[429, 500, 502, 503, 504])
+ses.mount("https://", HTTPAdapter(max_retries=retries))
+
+
+def make_request(url):  # makes a request using the session configured previously
+    return ses.get(url)
+
+
+def scrap_user_posts(
+    posts, thing, user
+):  # Process data from the post published by the user on r/spain
     top_matter = thing.find("div", attrs={"class": "top-matter"})
     title_all = top_matter.find("a", attrs={"class": "title"})
     title = title_all.text
@@ -35,7 +48,10 @@ def scrap_user_posts(posts, thing, user): # Process data from the post published
             }
         )
 
-def scrap_user_comments(comments, thing, user): # Process data from the comments published by the user on r/spain
+
+def scrap_user_comments(
+    comments, thing, user
+):  # Process data from the comments published by the user on r/spain
     parent = thing.find("p", attrs={"class": "parent"})
     if parent != None:
         title = parent.find("a", attrs={"class": "title"})
@@ -45,9 +61,7 @@ def scrap_user_comments(comments, thing, user): # Process data from the comments
         tagline = entry.find("p", attrs={"class": "tagline"})
         date = tagline.find("time")["title"]
 
-        usertext_body = entry.find(
-            "div", attrs={"class": "usertext-body"}
-        )
+        usertext_body = entry.find("div", attrs={"class": "usertext-body"})
 
         if usertext_body != None:
             md = usertext_body.find("div", attrs={"class": "md"})
@@ -63,49 +77,41 @@ def scrap_user_comments(comments, thing, user): # Process data from the comments
                     }
                 )
 
-def obtain_users_list_from_postList(users_list, posts): # Extracts a list of usernames from the html corresponding to posts passed
-    for post in posts:
-        user = post.find("a", attrs={"class": "author"})
-        if user != None:
-            users_list.append(user.text)
-    users_list = list(dict.fromkeys(users_list))
 
-def obtain_post_links_from_postList(posts_list, posts): # Extracts a list of post links from the html corresponding to posts passed
-    for post in posts:
-        top_matter = post.find("div", attrs={"class":"top-matter"})
-        if top_matter != None:
-            buttons = top_matter.find("ul", attrs={"class":"buttons"})
-            if buttons != None:
-                comments = buttons.find("a", attrs={"data-event-action":"comments"})
-                posts_list.append(comments['href'])
-    posts_list = list(dict.fromkeys(posts_list))
+def process_post_comment_data_from_userList(
+    posts, comments, html, user, subreddit
+):  # Proces a given html to scrap a user posts and comments in a given subredit
+    siteTable = html.find("div", attrs={"id": "siteTable"})
+    if siteTable != None:
+        things = siteTable.findAll("div", attrs={"class": "thing"})
 
-def obtain_user_karma(karma, html): # 
+        for thing in things:
+            if thing["data-subreddit"] == subreddit:
+                if thing["data-type"] == "link":
+                    scrap_user_posts(posts, thing, user)
+                elif thing["data-type"] == "comment":
+                    scrap_user_comments(comments, thing, user)
+
+
+def obtain_user_karma(
+    karma, html
+):  # Extracts the total user karma (post+comments karma) from a given html
     side = html.find("div", attrs={"class": "side"})
     if side != None:
         post_karma = side.find("span", attrs={"class": "karma"}).text
         comment_karma = side.find("span", attrs={"class": "comment-karma"}).text
         karma = int(post_karma.replace(",", "")) + int(comment_karma.replace(",", ""))
 
-def process_post_comment_data_from_userList(posts, comments, html, user):
-    siteTable = html.find("div", attrs={"id": "siteTable"})
-    if siteTable != None:
-        things = siteTable.findAll("div", attrs={"class": "thing"})
 
-        for thing in things:
-            if thing["data-subreddit"] == "spain":
-                if thing["data-type"] == "link":
-                    scrap_user_posts(posts, thing, user)
-                elif thing["data-type"] == "comment":
-                    scrap_user_comments(comments, thing, user)
-
-def process_user_data(user_data_json, base_url_user, users_list):
+def process_user_data(
+    user_data_json, base_url_user, users_list, subreddit
+):  # Creates the URL for a user profile from a given username and scraps itś karma, posts and comments in r/spain
     for user in users_list:
         user_url = base_url_user + user
         time.sleep(1)
-        res = requests.get(user_url)
+        res = make_request(user_url)
         html = BeautifulSoup(res.text, "html.parser")
-        
+
         # Karma
         karma = 0
         obtain_user_karma(karma, html)
@@ -113,16 +119,40 @@ def process_user_data(user_data_json, base_url_user, users_list):
         # Posts & Comments
         posts = []
         comments = []
-        process_post_comment_data_from_userList(posts, comments, html, user)
+        process_post_comment_data_from_userList(posts, comments, html, user, subreddit)
         user_data_json.append(
             {"username": user, "karma": karma, "posts": posts, "comments": comments}
         )
 
 
+def obtain_users_list_from_postList(
+    users_list, posts
+):  # Extracts a list of usernames from the html corresponding to posts passed
+    for post in posts:
+        user = post.find("a", attrs={"class": "author"})
+        if user != None:
+            users_list.append(user.text)
+    users_list = list(dict.fromkeys(users_list))
+
+
+def obtain_post_links_from_postList(
+    posts_list, posts
+):  # Extracts a list of post links from the html corresponding to posts passed
+    for post in posts:
+        top_matter = post.find("div", attrs={"class": "top-matter"})
+        if top_matter != None:
+            buttons = top_matter.find("ul", attrs={"class": "buttons"})
+            if buttons != None:
+                comments = buttons.find("a", attrs={"data-event-action": "comments"})
+                posts_list.append(comments["href"])
+    posts_list = list(dict.fromkeys(posts_list))
+
+
 base_url = "https://old.reddit.com/r/spain/new/"
 base_url_user = "https://old.reddit.com/user/"
+subreddit = "spain"
 
-res = requests.get(base_url)
+res = make_request(base_url)
 html = BeautifulSoup(res.text, "html.parser")
 siteTable = html.find("div", attrs={"id": "siteTable"})
 posts = siteTable.findAll("div", attrs={"class": "link"})
@@ -132,10 +162,9 @@ obtain_users_list_from_postList(users_list, posts)
 print(users_list)
 
 user_data_json = []
-process_user_data(user_data_json, base_url_user, users_list)
+process_user_data(user_data_json, base_url_user, users_list, subreddit)
 print(user_data_json)
 
 posts_list = []
 obtain_post_links_from_postList(posts_list, posts)
 print(posts_list)
-
