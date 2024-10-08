395ad0b2f0165e1d267067b0c9942d0ffb40c0eb
PsychO <datanc01@ucm.es>
2024-10-09 00:20:20 +0200

    Added ability to scrap multiple pages on a single execution and filter duplicate users

--- P1/src/p1/main.py
+++ P1/src/p1/main.py
@@ -10,6 +10,7 @@ _PERIOD = 5
 _CALLS = 2
 _BASE_URL = "https://old.reddit.com"
 _BASE_URL_USER = _BASE_URL + "/user/"
+_ITEMS_PER_PAGE = 25
 
 
 @sleep_and_retry
@@ -45,32 +46,33 @@ def obtain_user_karma(
         karma = int(post_karma.replace(",", "")) + int(comment_karma.replace(",", ""))
         return karma
     else:
-        # print("[DEBUG]: User account is NSFW, therefore, can´t be scrapped")
         return -1
 
 
 def process_user_data(
-    user_data, users_list, subreddit
+    user_data, users_list, users_all, subreddit
 ):  # Creates the URL for a user profile from a given username and scraps its karma, posts and comments in the subreddit
     for user in users_list:
-        user_url = _BASE_URL_USER + user
-        html = make_request(user_url)
+        if user not in users_all:
+            users_all.append(user)
+            user_url = _BASE_URL_USER + user
+            html = make_request(user_url)
 
-        # Karma
-        karma = obtain_user_karma(html)
+            # Karma
+            karma = obtain_user_karma(html)
 
-        # Posts & Comments
-        posts = []
-        comments = []
-        obtain_user_posts_comments(posts, comments, html, subreddit)
-        user_data.append(
-            {
-                "username": user,
-                "karma": karma,
-                "posts": posts,
-                "comments": comments
-            }
-        )
+            # Posts & Comments
+            posts = []
+            comments = []
+            obtain_user_posts_comments(posts, comments, html, subreddit)
+            user_data.append(
+                {
+                    "username": user,
+                    "karma": karma,
+                    "posts": posts,
+                    "comments": comments
+                }
+            )
 
 
 def process_comment_data(comment_data, comments, post_url):
@@ -132,21 +134,13 @@ def process_post_data(post_data, comment_data, posts_list):
                 else:
                     author = "Deleted Account"
 
-                # # Media
-                # post_link = entry.find("a", attrs={"class":"post-link"})
-                # if post_link != None and 'src' in post_link.attrs:
-                #     media = post_link['src']
-                # else:
-                #     media = ""
-
                 post_data.append(
                     {
                         "post": post_url,
                         "title": title,
                         "date": date,
                         "description": desc,
-                        "author": author,
-                        #"media": media
+                        "author": author
                     }
                 )
 
@@ -164,13 +158,48 @@ def save_file(file, data):  # Saves the given data in json format to the given f
     with open(file + ".json", "w", encoding="utf-8") as f:
         json.dump(data, f, ensure_ascii=False, indent=3)
 
+def process_page(user_data, post_data, comment_data, page_url, users_all):
+    print(" Scrapping subreddit: " + page_url)
+    print("---------------------------------------------------------------------------------")
+    html = make_request(page_url)
+
+    siteTable = html.find("div", attrs={"id": "siteTable"})
+    if siteTable != None:
+        things = siteTable.findAll("div", attrs={"class": "thing"})
+
+        users = []
+        posts = []
+
+        for thing in things:
+            if "data-permalink" in thing.attrs:
+                posts.append(thing["data-permalink"])
+
+            if "data-author" in thing.attrs:
+                users.append(thing["data-author"])
+
+        users = list(dict.fromkeys(users))
+        posts = list(dict.fromkeys(posts))
+
+        print(" Obtaining user data")
+        process_user_data(user_data, users, users_all, _subreddit)
+
+        print(" Obtaining posts data")
+        process_post_data(post_data, comment_data, posts)
+
+        return things[-1]['data-fullname']
+    else:
+        print("[ERROR]: Page: " + page_url + " didn´t have a div with id siteTable (?)")
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    "-t", "--type", help="Type of scrapping: ['', 'new', 'rising', 'top']", default="new"
+    "-t", "--type", help="Category of scrapping for the given subreddit: ['', 'new', 'rising', 'top']", default="new"
 )
 parser.add_argument(
     "-s", "--subreddit", help="Subreddit to scrap: spain, europe, spicypillows...", default="spain"
+)
+parser.add_argument(
+    "-n", "--number", help="Number of pages to scrap from the given subreddit and category", default=5
 )
 parser.add_argument(
     "-o",
@@ -183,45 +212,35 @@ args = parser.parse_args()
 _type = args.type
 _subreddit = args.subreddit
 _output_file = args.output
+_number_of_pages = args.number
 
 print("Reddit Scrapper")
-print(" Scrapping subreddit: " + _BASE_URL + "/r/" + _subreddit + "/" + _type + "/")
 print(" Outputting data to: " + _output_file +"_data_type.json")
 print(
     "---------------------------------------------------------------------------------"
 )
 
-html = make_request(_BASE_URL + "/r/" + _subreddit + "/" + _type + "/")
-
-siteTable = html.find("div", attrs={"id": "siteTable"})
-things = siteTable.findAll("div", attrs={"class": "thing"})
-
-users = []
-posts = []
-for thing in things:
-    if "data-permalink" in thing.attrs:
-        posts.append(thing["data-permalink"])
-    # else:
-        # print("[DEBUG]: tag (data-permalink) not found -> ???")
-
-    if "data-author" in thing.attrs:
-        users.append(thing["data-author"])
-    # else:
-        # print("[DEBUG]: tag (data-author) not found -> User account was deleted")
-
-users = list(dict.fromkeys(users))
-posts = list(dict.fromkeys(posts))
-
-print(" Obtaining user data")
 user_data = []
-process_user_data(user_data, users, _subreddit)
-print(" Saving data to: user_" + _output_file + ".json...")
-save_file("user_" + _output_file, user_data)
-
-print(" Obtaining posts data")
 post_data = []
 comment_data = []
-process_post_data(post_data, comment_data, posts)
+
+users_all = []
+
+page_url = _BASE_URL + "/r/" + _subreddit + "/" + _type + "/"
+print("Page 1 out of " + str(_number_of_pages))
+last_post_id = process_page(user_data, post_data, comment_data, page_url, users_all)
+print(last_post_id)
+
+pages_processed = 1
+while pages_processed < int(_number_of_pages):
+    print("Page " + str(pages_processed+1) + " out of " + str(_number_of_pages))
+    page_url = _BASE_URL + "/r/" + _subreddit + "/" + _type + "/" + "?count=" + str(pages_processed * _ITEMS_PER_PAGE) + "&after=" + last_post_id
+    process_page(user_data, post_data, comment_data, page_url, users_all)
+    pages_processed += 1
+
+
+print(" Saving data to: user_" + _output_file + ".json...")
+save_file("user_" + _output_file, user_data)
 print(" Saving posts to: post_" + _output_file + ".json...")
 save_file("post_" + _output_file, post_data)
 print(" Saving comments to: comment_" + _output_file + ".json...")
