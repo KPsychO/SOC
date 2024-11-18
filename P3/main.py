41a9fd5e15abb17648e22fa49c94e64b7ce1bf12
PsychO <datanc01@ucm.es>
2024-11-18 21:35:37 +0100

    p3 start

---
+++ P3/main.py
@@ -0,0 +1,10 @@
+# parametros(click): numero de nodos para ser hub, nombre del fichero a procesar
+# invocar al py que toca por parametro
+# prints bonitos
+# calculo de carga y estatisticas de tiempo de ejecucion
+#
+#
+#
+#
+#
+#
