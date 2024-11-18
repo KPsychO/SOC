303bd24d0ba5cf9c8cdd7be5b42effe1fc9d4c31
PsychO <datanc01@ucm.es>
2024-11-18 23:19:42 +0100

    done? missing some functions on process_data.py, main should be good

--- P3/main.py
+++ P3/main.py
@@ -1,14 +1,35 @@
 import click
 from process_data import process_edgelist
 
+def print_info(n_hub, adjacency_list, n_edges, graph_degree_distribution, cluster_coeficiency_distribution):
+    print(n_hub)
+    print('Graph degree distribution:')
+    for n in graph_degree_distribution.keys():
+        print('     ' + str(n) + ' :    '+  str(graph_degree_distribution[n]))
+
+    print('Cluster coeficiency distribution:')
+    for n in cluster_coeficiency_distribution.keys():
+        print('     ' + str(n) + ' :    '+  str(cluster_coeficiency_distribution[n]))
+
+    print('Hubs with more than ' + str(n_hub) + ' edges:')
+    for n in adjacency_list.keys():
+        if len(adjacency_list[n]) > int(n_hub):
+            print('     ' + str(n) + ' :    '+  str(len(adjacency_list[n])))
+    
+    print('number of nodes:     ' + str(len(adjacency_list)))
+    print('number of edges:     ' + str(n_edges))
+
 @click.command()
 @click.argument('filename')
-@click.option('-n', '--n_hub', type=int)
-
+@click.argument('n_hub')
 def main(filename, n_hub):
-    process_edgelist(filename, n_hub)
+    
+    adjacency_list, n_edges, graph_degree_distribution, cluster_coeficiency_distribution = process_edgelist(filename)
+
+    print_info(n_hub, adjacency_list, n_edges, graph_degree_distribution, cluster_coeficiency_distribution)
 
 main()
+
 
 # parametros(click): numero de nodos para ser hub, nombre del fichero a procesar
 # invocar al py que toca por parametro
--- P3/process_data.py
+++ P3/process_data.py
@@ -14,14 +14,29 @@ def adjacencylist_from_edgelistFile(filename):
 
     return adjacency_list, n_edges
 
-def process_edgelist(filename, n_hub):
+def cluster_coeficiency_distribution_from_adjacencylist(adjacency_list):
+    cluster_coeficiency_distribution = {}
+
+    # TODO: fill that shit up
+
+    return cluster_coeficiency_distribution
+
+def graph_degree_distribution_from_adjacencylist(adjacency_list):
+    graph_degree_distribution = {}
+
+    for n in adjacency_list.keys():
+        degree = len(adjacency_list[n])
+        if degree not in graph_degree_distribution:
+            graph_degree_distribution[degree] = 1
+        else:
+            graph_degree_distribution[degree] += 1
+    
+    return graph_degree_distribution
+
+def process_edgelist(filename):
     # Convert the edgelist to adjacency list
     adjacency_list, n_edges = adjacencylist_from_edgelistFile(filename)
+    graph_degree_distribution = graph_degree_distribution_from_adjacencylist(adjacency_list)
+    cluster_coeficiency_distribution = cluster_coeficiency_distribution_from_adjacencylist(adjacency_list)
 
-    print('Hubs with more than ' + str(n_hub) + ' edges:')
-    for n in adjacency_list.keys():
-        if len(adjacency_list[n]) > n_hub:
-            print('     ' + str(n) + ' :    '+  str(len(adjacency_list[n])))
-
-    print('number of nodes:     ' + str(len(adjacency_list)))
-    print('number of edges:     ' + str(n_edges))
+    return adjacency_list, n_edges, graph_degree_distribution, cluster_coeficiency_distribution
